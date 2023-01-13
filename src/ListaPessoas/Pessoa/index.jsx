ec2f650e6c7d7473ea4842322b2f4400529efa42
Joao Lino <linusmannuel@gmail.com>
2023-01-13 17:17:19 +0100

    feat: Make Component Pessoa

---
+++ src/ListaPessoas/Pessoa/index.jsx
@@ -0,0 +1,7 @@
+const Pessoa = (name) => {
+  return (
+    <li>{name}</li>
+  )
+}
+
+export default Pessoa
---
+++ src/ListaPessoas/index.jsx
@@ -0,0 +1,11 @@
+import Pessoa from "./Pessoa"
+
+const ListaPessoas = () => {
+  return (
+    <ul>
+      <Pessoa />
+    </ul>
+  )
+}
+
+export default ListaPessoas
